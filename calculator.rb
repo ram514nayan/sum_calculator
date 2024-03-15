4fc86be049aed9fedbba8e3e3d9ba0e0573c01ca
ram514nayan <ram514vishwakarma@gmail.com>
2024-03-15 17:11:17 +0530

    calculate sum with multiple new line, last line empty and negative numbers with new lines

--- calculator.rb
+++ calculator.rb
@@ -5,9 +5,9 @@ class Calculator
     @regex_str = /^[\d\(\)\-#{delimiter}]+$/
 
     print "Input: #{str}: || "
+    validate_last_line str
     numbers = @input_string.split(delimiter).map(&:to_i)
     validate_string
-    validate_last_line
     negative_numbers numbers
 
     if @errors.empty?     
@@ -20,12 +20,13 @@ class Calculator
   def negative_numbers numbers
     negative_nums = numbers.select(&:negative?)
     begin
-      raise "Negative numbers" if negative_nums.any?
+      raise "Negative numbers are not allowed:" if negative_nums.any?
     rescue StandardError => e
-      @errors.concat("#{e} are not allowed: #{negative_nums.join(', ')}, ")
+      @errors.concat("#{e} #{negative_nums.join(', ')}, ")
     end
   end
 
+  # Return used delimiter and modified string
   def check_delimiter str
     if(str[0..1].include?("//"))
       delimiter = str[2]
@@ -34,9 +35,12 @@ class Calculator
     else
       delimiter = ','
     end
+    nl_split = str.gsub(/-\n+/, "#{delimiter}-").split("\n")
+    str = nl_split.join(delimiter)
     return str, delimiter
   end
 
+  #This will validate delimiter should not be number it also can cause error
   def validate_delimiter del
     begin
       raise 'Invalid input: Delimiter should not be number' unless del.to_s.match? /^(?![0-9]+$).*/
@@ -45,7 +49,7 @@ class Calculator
     end
   end
 
-  # Testing for only accepting numbers with delimiters
+  # Validating for only accept numbers with delimiters
   def validate_string
     begin
       raise "Invalid input: Only allowed specified delimiter as (//delimiter) with integer" unless @input_string.match @regex_str
@@ -54,9 +58,10 @@ class Calculator
     end
   end
 
-  def validate_last_line
+  # As mention (The following input is invalid: "1,\n") any last line should not be empty
+  def validate_last_line str
     begin
-      raise "Invalid input: Last line should not be empty" if @input_string[-2..-1].include?("\n")
+      raise "Invalid input: Last line should not be empty" if str[str.length-1] == "\n"
     rescue StandardError => e
       @errors.concat("#{e}, ")
     end
@@ -80,5 +85,5 @@ calculator.add_numbers "//A10A15A7A14"
 #With ifferent delimiter, should not be number
 calculator.add_numbers "//910915979-14"
 
-#Validation for last new line
-calculator.add_numbers "//q1q-2q400q5\n"
+#Validation for last new line with multiple new lines and negative numbers
+calculator.add_numbers "23,43,34-\n\n\n234\n-21,45,\n,-\n50\n"
